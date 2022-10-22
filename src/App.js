4d75d3df91195eb1d2e028947767f784fde1c06e
VijayKumarReddyTalakola <109469574+VijayKumarReddyTalakola@users.noreply.github.com>
2022-10-22 09:33:48 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+import DataFetching from "./components/DataFetching";
+export default function App() {
+  return (
+    <div className="App">
+      <DataFetching />
+    </div>
+  );
+}
